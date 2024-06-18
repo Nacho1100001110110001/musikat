df05e4793a4c6ade48dc1081f2e2f5a71565ed39
Nacho1100001110110001 <ignacio.rivas.u@gmail.com>
2024-06-18 11:52:19 -0400

    profile component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgxBootstrapIconsModule, allIcons } from 'ngx-bootstrap-icons';
+import { HeaderComponent } from './components/header/header.component';
+import { FriendsMenuComponent } from './components/friends-menu/friends-menu.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FriendsMenuComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgxBootstrapIconsModule.pick(allIcons),
+    RouterModule.forRoot([
+      {path: 'perfil', component: ProfileComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
